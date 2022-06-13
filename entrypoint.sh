894115c3e925b38a902652b851f4152f91546475
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-13 13:14:51 -0500

    added build_testing and coverage tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh -l
-cmake -B `pwd`/build -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE
-cmake --build `pwd`/build --parallel $(nproc) --config $INPUT_BUILD_TYPE
+cmake -B `pwd`/build -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DBUILD_TESTING=$INPUT_BUILD_TESTS -DCODE_COVERAGE=$INPUT_BUILD_COVERAGE
+# cmake --build `pwd`/build --parallel $(nproc) --config -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE 
+make -j$(nproc) -C build/
+
+if $INPUT_BUILD_TESTS; then
+  make test -C build/
+fi
+
+if $INPUT_BUILD_COVERAGE; then
+  # make coverage -j$(nproc) -C build/
+  lcov --capture --directory build/ --output-file coverage.info
+  lcov --remove coverage.info '*/external/include/*' --output-file coverage.info
+  lcov --remove coverage.info '/usr/*' --output-file coverage.info 
+  lcov --list coverage.info
+fi
