61dd4c6e0faf3113d6a156314aebec47f7c45204
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-14 06:28:40 -0500

    silent steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -l
 cmake -B `pwd`/build -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DBUILD_TESTING=$INPUT_BUILD_TESTS -DCODE_COVERAGE=$INPUT_BUILD_COVERAGE
 # cmake --build `pwd`/build --parallel $(nproc) --config -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE 
-make -j$(nproc) -C build/
+make -j$(nproc) -C build/ --no-print-directory
 
 if $INPUT_BUILD_TESTS; then
-  make test -C build/
+  make test -C build/ --no-print-directory
 fi
 
 if $INPUT_BUILD_COVERAGE; then
   # make coverage -j$(nproc) -C build/
-  lcov --capture --directory build/ --output-file coverage.info
-  lcov --remove coverage.info '*/external/include/*' --output-file coverage.info
-  lcov --remove coverage.info '/usr/*' --output-file coverage.info 
+  lcov --capture --directory build/ --output-file coverage.info --quiet
+  lcov --remove coverage.info '*/external/include/*' --output-file coverage.info --quiet
+  lcov --remove coverage.info '/usr/*' --output-file coverage.info --quiet
   lcov --list coverage.info
 fi
