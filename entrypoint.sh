241a30ac7d1bcb97f13bd089032059d348513754
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-14 10:55:19 -0500

    added exit code if commands fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
+set -e
+
 cmake -B `pwd`/build -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DBUILD_TESTING=$INPUT_BUILD_TESTS -DCODE_COVERAGE=$INPUT_BUILD_COVERAGE
-# cmake --build `pwd`/build --parallel $(nproc) --config -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE 
+
+# cmake --build `pwd`/build --parallel $(nproc) --config -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE
 make -j$(nproc) -C build/ --no-print-directory
 
 if $INPUT_BUILD_TESTS; then
